f89d22c62ff602fa05750e56a197e6bc4e2150d8
Michael <michael@topmiller@gmail.com>
2018-06-07 16:48:19 -0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,49 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+// 	str.split('');
+
+// 	let obj = {};
+// 	for (let character in str) {
+// 		if (obj[str[character]]) {
+// 			obj[str[character]] += 1;
+// 		} else {
+// 			obj[str[character]] = 1;
+// 		}
+// 	}
+// 	console.log(obj);
+
+// 	let maxChar = '';
+// 	let maxVal = 0;
+// 	for (let key in obj) {
+// 		if (obj[key] > maxVal) {
+// 			maxVal = obj[key];
+// 			maxChar = key;
+// 		}
+// 	}
+// 	return maxChar;
+// }
