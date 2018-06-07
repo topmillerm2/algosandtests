4b569bd24f6d9f1df3a6952222bbf1a0e9263a83
Michael <michael@topmiller@gmail.com>
2018-06-07 15:02:31 -0400

    palindromes

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,19 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+// 	// const str2  = str.split('').reverse().join('')
+// 	let str2 = '';
+// 	for (let character of str) {
+// 		str2 = character + str2;
+// 	}
+// 	return str === str2;
+// }
